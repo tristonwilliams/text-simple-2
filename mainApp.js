09f10eb8f83fb3df72887e8be8f98f278ccb0c2a
tristonwilliams <triston.williams@salesforce.com>
2018-07-02 10:02:28 +1000

    Update mainApp.js

--- mainApp.js
+++ mainApp.js
@@ -1,3 +1,6 @@
+// JavaScript Document
+
+
 if (window.self === window.top) {
     document.body.innerText = 'This application is for use in the Salesforce Marketing Cloud Content Builder only!';;
  }
@@ -21,12 +24,16 @@ let saveData = () => {
     imgData.ImageURL = document.getElementById('ImageURL').value;
     imgData.Text = document.getElementById('Text').value;
 	imgData.Textcolor = document.getElementById('Textcolor').value;
-
+	
+	
+	
 
     sdk.setData(imgData, (data) => {
         
 		
-		 let content = "<table width='100%' border='0' cellspacing='0' cellpadding='0'> <tbody> <tr> <td style='background-repeat:no-repeat;background-position: center;' background='" + imgData.ImageURL + "' bgcolor='#ffffff' width='600' height='" + imgData.ImgHeight + "' valign='top'> <!--[if gte mso 9]> <v:rect xmlns:v='urn:schemas-microsoft-com:vml' fill='true' stroke='false' style='background-repeat:no-repeat; width:600px;height:" + imgData.ImgHeight + "px;'> <v:fill type='tile' src='" + imgData.ImageURL + "' color='#ffffff' /> <v:textbox inset='0,0,0,0'> <![endif]--> <div> <table width='100%' border='0' cellspacing='0' cellpadding='0'> <tbody> <tr> <td width='30' align='left' valign='top' style='font-size: 0%;' class='mobile-hidden'></td> <td align='left' valign='top'><table width='100%' border='0' cellspacing='0' cellpadding='0'> <tbody> <tr> <td align='left' valign='top' style='padding-top: 95px;color: " + imgData.Textcolor + "; padding-left: 100px; padding-right: 100px; font-size: 48px;'><center>" + imgData.Text + "</center></td> </tr> </tbody> </table></td> <td width='30' align='left' valign='top' style='font-size: 0%;'></td> </tr> </tbody> </table> </div> <!--[if gte mso 9]> </v:textbox> </v:rect> <![endif]--> </td> </tr> </tbody> </table>";    
+		 let content = "<table width='100%' border='0' cellspacing='0' cellpadding='0'> <tbody> <tr> <td style='background-repeat:no-repeat;background-position: center;' background='" + imgData.ImageURL + "' bgcolor='#ffffff' width='600' height='" + imgData.ImgHeight + "' valign='top'> <!--[if gte mso 9]> <v:rect xmlns:v='urn:schemas-microsoft-com:vml' fill='true' stroke='false' style='background-repeat:no-repeat; width:600px;height:" + imgData.ImgHeight + "px;'> <v:fill type='tile' src='" + imgData.ImageURL + "' color='#ffffff' /> <v:textbox inset='0,0,0,0'> <![endif]--> <div> <table width='100%' border='0' cellspacing='0' cellpadding='0'> <tbody> <tr> <td width='30' align='left' valign='top' style='font-size: 0%;' class='mobile-hidden'></td> <td align='left' valign='top'><table width='100%' border='0' cellspacing='0' cellpadding='0'> <tbody> <tr> <td align='left' valign='top' style='padding-top: 95px;color: " + imgData.Textcolor + "; padding-left: 100px; padding-right: 100px; font-size: 48px;'><center>" + imgData.Text + "</center></td> </tr> </tbody> </table></td> <td width='30' align='left' valign='top' style='font-size: 0%;'></td> </tr> </tbody> </table> </div> <!--[if gte mso 9]> </v:textbox> </v:rect> <![endif]--> </td> </tr> </tbody> </table>";
+
+        
 
         //check for ampscript
         if (content.search('%%') != -1) {
@@ -42,20 +49,28 @@ let saveData = () => {
 
 let fetchData = () => {
 
+  Alert(imgData);
+
     sdk.getData((data) => {
         if (Object.keys(data).length > 0) {
-            imgData = data;        
+            imgData = data;
+         Alert(imgData);
 			
 	document.getElementById('ImgHeight').value = imgData.ImgHeight;
       document.getElementById('ImageURL').value = imgData.ImageURL;
     document.getElementById('Text').value = imgData.Text;
-	document.getElementById('Textcolor').value = imgData.Textcolor;		
-     
+	document.getElementById('Textcolor').value = imgData.Textcolor;
+			
+     Alert(imgData);
         }
 		
     });
 	console.log(JSON.stringify(imgData));
-
+Alert(imgData);
 }
+
+
+
+
 window.onload = fetchData;
 window.onchange = saveData;
